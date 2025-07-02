a18156f6f1ddd98c1311c50547ab8e7d2fbf5dc5
Salil Lakra <salillakra.dev@gmail.com>
2025-07-02 22:38:14 +0530

    chore: update dependencies and improve configuration
    - Added @prisma/client, @tanstack/react-query, date-fns, framer-motion, react-share-social, sonner, and uuid to dependencies.
    - Updated versions of @radix-ui/react-slot and next-themes.
    - Updated tailwindcss-animate plugin import in tailwind.config.ts.
    - Fixed path for @components in tsconfig.json.

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "res.cloudinary.com",
-			},
-			{
-				protocol: "https",
-				hostname: "wallpapers.com",
-			},
-			{
-				protocol: "https",
-				hostname:"easy-peasy.ai"
-			}
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "wallpapers.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatar.iran.liara.run",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
